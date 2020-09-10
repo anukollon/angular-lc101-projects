3ddcdd96941d0ac3be0267f933488fe1ae34d47a
unknown <anukollon@gmail.com>
2020-09-10 12:14:49 -0500

    Part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cdn.pixabay.com/photo/2020/05/09/05/30/hirsch-5148320_960_720.jpg';
+  image3 = 'https://cdn.pixabay.com/photo/2020/09/04/16/18/mountains-5544365_1280.jpg';
 
   constructor() { }
 
